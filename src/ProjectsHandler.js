a229df45afc033a2b95703c4420178be247cf452
dowhep <caogangshen@gmail.com>
2022-06-18 14:30:18 -0700

    added links for projects and parsing accordingly

--- src/ProjectsHandler.js
+++ src/ProjectsHandler.js
@@ -1,3 +1,6 @@
+import ghimg from './Icons/githubwhite.png';
+import itchimg from './Icons/itchiowhite.svg';
+
 // load data
 const data = require('./projects.json');
 // const len = Object.keys(data).length;
@@ -6,14 +9,31 @@ const data = require('./projects.json');
 const projects = [];
 let k = 1;
 
+// icon generation
+const generateLinkIcons = (github, itchio) => {
+  return (
+    <div className="proj_icons">
+      {github ? (
+        <a href={github}>
+          <img src={ghimg} className="proj_icon" />
+        </a>
+      ) : null}
+      {itchio ? (
+        <a href={itchio}>
+          <img src={itchimg} className="proj_icon" />
+        </a>
+      ) : null}
+    </div>
+  );
+};
+
 data.forEach((proj) => {
   projects.push(
     <div className="proj_container" key={k++}>
-      <a href={proj.url} className="proj_name">
-        {proj.name}
-      </a>
+      <div className="proj_name">{proj.name}</div>
       <div className="proj_tech">{proj.tech}</div>
       <div className="proj_desc">{proj.desc}</div>
+      {generateLinkIcons(proj.github, proj.itchio)}
     </div>
   );
 });
